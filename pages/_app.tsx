c141cb8b635848918f5dae7f4eef5947643f6b2f
Dao Nguyen <zendobk@gmail.com>
2024-10-11 14:30:10 +0700

    add meta fields

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css';
 import { GoogleTagManager } from '@next/third-parties/google';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <title>My Blog</title>
+      </Head>
       <Component {...pageProps} />;
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="My Blog" />
+        <meta
+          name="description"
+          content="Stay updated with the latest trends and insights in technology, programming, and software development. Join our community of tech enthusiasts and elevate your coding skills with expert tips and tutorials."
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
